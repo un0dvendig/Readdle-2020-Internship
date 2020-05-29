d583402cb0df3fb0186b2ac4f69316a904735a4f
Eugene Ilyin <evgen.ilyin@gmail.com>
2020-05-29 14:35:19 +0300

    Setup.

---
+++ Readdle 2020/Resources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Readdle 2020
+//
+//  Created by Eugene Ilyin on 29.05.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+    
+    var window: UIWindow?
+
+    // MARK: - UIApplicationDelegate methods
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
